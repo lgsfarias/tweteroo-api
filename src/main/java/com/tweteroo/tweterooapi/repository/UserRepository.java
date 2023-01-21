73551bf49d6b036fef013baba188a34c9075475d
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-21 12:27:39 -0300

    feat: add findByUsername to user repository

--- src/main/java/com/tweteroo/tweterooapi/repository/UserRepository.java
+++ src/main/java/com/tweteroo/tweterooapi/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.tweteroo.tweterooapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo.tweterooapi.model.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
-
+  List<UserModel> findByUsername(String username);
 }

947b36a1664543ca1a90537db4d9d3d92806f11a
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-21 10:29:00 -0300

    feat: create Tweet model

---
+++ src/main/java/com/tweteroo/tweterooapi/model/TweetModel.java
@@ -0,0 +1,36 @@
+package com.tweteroo.tweterooapi.model;
+
+import com.tweteroo.tweterooapi.dto.TweetDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class TweetModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(nullable = false, length = 50)
+  private String username;
+
+  @Column(nullable = false, length = 200)
+  private String avatar;
+
+  @Column(nullable = false, length = 280)
+  private String text;
+
+  public TweetModel(TweetDTO tweetDTO) {
+    this.username = tweetDTO.username();
+    this.avatar = tweetDTO.avatar();
+    this.text = tweetDTO.text();
+  }
+}

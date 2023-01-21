e5878e84729d4481b30951583827ea9b113bd889
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-21 12:29:21 -0300

    chore: change cross origin and response statusCode

--- src/main/java/com/tweteroo/tweterooapi/controller/UserController.java
+++ src/main/java/com/tweteroo/tweterooapi/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.tweteroo.tweterooapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 import com.tweteroo.tweterooapi.dto.UserDTO;
 import com.tweteroo.tweterooapi.model.UserModel;
@@ -14,6 +16,7 @@ import com.tweteroo.tweterooapi.repository.UserRepository;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/auth/sign-up")
 public class UserController {
 
@@ -21,6 +24,7 @@ public class UserController {
   private UserRepository repository;
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public String signUp(@RequestBody @Valid UserDTO userDTO) {
     repository.save(new UserModel(userDTO));
     return "OK";

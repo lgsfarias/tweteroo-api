0bbc32643874960cf42fa357cb0c35a0e3f898ed
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-21 12:25:08 -0300

    feat: create tweet DTO

--- src/main/java/com/tweteroo/tweterooapi/dto/TweetDTO.java
+++ src/main/java/com/tweteroo/tweterooapi/dto/TweetDTO.java
@@ -3,8 +3,5 @@ package com.tweteroo.tweterooapi.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record TweetDTO(
-
-        @NotBlank(message = "Username is required") String username,
-
-        @NotBlank(message = "Text is required") String text) {
+                @NotBlank(message = "Text is required") String text) {
 }
